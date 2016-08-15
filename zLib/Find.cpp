e2d7c244ee7e1f3db09aa3e0fb924252f3c98446
Samuel Ghinet <samuel.ghinet@gmail.com>
2016-08-15 15:30:26 +0300

    Add files for Find
    NOTE: Not yet implemented on Windows.
    The content of project ZSearch, files ZSearch.cpp/.h was moved there, since it is API-related.

---
+++ zLib/Find.cpp
@@ -0,0 +1,66 @@
+/*
+*  Copyright 2015 Samuel Ghineț
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+
+#include "Find.h"
+
+#include <iostream>
+#include <memory>
+#include <cstring>
+#include <errno.h>
+
+using namespace Zen;
+
+#ifdef __linux__
+#include <dirent.h>
+
+Results Find::operator()()
+{
+    Results r;
+
+    DIR* dirStream = opendir(m_dirPath.c_str());
+    if (!dirStream) {
+        std::cerr << "opendir: " << strerror(errno) << std::endl;
+        return r;
+    }
+
+    while (dirent* dirEntry = readdir(dirStream)) {
+        ResultItem item;
+        item.fullName = dirEntry->d_name;
+
+        if (item.fullName != "." && item.fullName != "..")
+            r.push_back(item);
+    }
+
+    if (-1 == closedir(dirStream)) {
+        std::cerr << "closedir: " <<  strerror(errno) << std::endl;
+        return r;
+    }
+
+    return r;
+}
+
+#else
+
+Results Find::operator()()
+{
+    if (m_dirPath.empty())
+        return ZSearch::Results{};
+
+    ResultItem item = { T("OneItem") };
+    return Results{ item };
+}
+
+#endif
---
+++ zLib/Find.h
@@ -0,0 +1,45 @@
+/*
+*  Copyright 2015 Samuel Ghineț
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "zLib.h"
+
+namespace Zen
+{
+    struct ZLIB_API ResultItem
+    {
+        /* fullName: default value - a) if result = not found / invalid; b) if filter, means 'any' */
+        std::tstring fullName;
+    };
+
+    typedef std::vector<ResultItem> Results;
+
+    class ZLIB_API Find
+    {
+    public:
+        Find() : m_dirPath(T(".")) {}
+        explicit Find(const std::tstring& path) : m_dirPath(path) {}
+
+        Results operator()();
+
+    private:
+        std::tstring m_dirPath;
+    };
+}
